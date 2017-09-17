2abe54655d1db9e922c2e3e0a3663589e9c92b3c
Cesc <francesc@libertium.net>
2017-09-17 19:55:07 +0100

    - Separamos el bus de datos dio en entradas y salidas: sfg_dbi y sfg_dbo - Agregamos una señal de salida oe_n para indicar que se debe direccionar la ROM del SFG01

--- hdl/jt-sfg01.v
+++ hdl/jt-sfg01.v
@@ -1,6 +1,10 @@
 module jt_sfg01(
 	input					rst,		// pin 25 		(MSX 15) RESET 
-	inout reg [ 7:0] 	dio, 		// pins 43-50 	IO(MSX 33-40) 	DATA BUS 	
+
+	//inout reg [ 7:0] 	dio, 		// pins 43-50 	IO(MSX 33-40) 	DATA BUS 	
+	output [ 7:0]     sfg_dbi, 		// pins 43-50 	IO(MSX 33-40) 	DATA BUS 	
+	input  [ 7:0]     sfg_dbo, 		// pins 43-50 	IO(MSX 33-40) 	DATA BUS 	
+
 	input  [15:0] 		addr, 	// pins 27-42	O (MSX 17-32)	ADDRESS BUS
 	input					clk,		// pin 52		O (MSX 42)		CLOCK 3.579545Mhz
 	// bus control
@@ -13,10 +17,10 @@ module jt_sfg01(
 	output				int_n,	// pin 18		I (MSX 08) 		Maskable INTERRUPT request, open collector signal
 	output				wait_n,	// pin 17		I (MSX 07) 		WAIT request, open collector signal
 	
-	output reg			oe_n,		// ROM OE_n
+	output 				oe_n,		// ROM OE_n 
 	
-	output  [15:0] 	xleft,
-	output  [15:0] 	xright
+	output  [15:0] 	left,
+	output  [15:0] 	right
 	
 	// midi
 	// input				midi_rx,
@@ -37,28 +41,30 @@ module jt_sfg01(
 // #3FF6 (W)	MIDI UART Command Register
 
 // glue logic
-// A14-A0 (32KB)
-// A13-A7 == 1..1	--> CE_n
-// A6-A3  == 1..1	--> CS_n
-wire ce_n = &addr[13:7]; // IC105 and IC106  Nota: no deberia ser 14:7 ?
+wire ce_n = &addr[13:7]; // IC105 and IC106  
+
 wire ic106_4 = ~(addr[6] & ce_n);
 wire ic106_2 = ~(addr[5] & addr[4]);
+
 wire ic107_3 = ~(~ic106_4 & ~ic106_2); // this is just an OR but I want to follow the schematic
+
 wire ic107_4 = ~(~ic107_3 & ~addr[3]);
 
-wire cs_n    = ~(~ic107_4 & ~slt3_n); // ic107_1
+wire cs_n    = ~(~ic107_4 & ~slt3_n);  // ic107_1
 
-assign busdir_n = ~(~iorq_n & ~mi);		// ic107_2
-assign wait_n	= 1'b1;
+assign busdir_n = ~(~iorq_n & ~mi);    // ic107_2
+assign wait_n	= 1'b1;                 // No usamos el wait
 
-wire opm_n = !(!cs_n && addr[2:1]==2'b00);	// OPM active when not CS and addr[2] =0 and addr[1] =0 ??
+// OPM_n	         == 0        --> CS_n --> YM2151
+wire opm_n = !(!cs_n && addr[2:1]==2'b00); // OPM active when CS (0x3FF0-0x3FF8) and addr[2:1] =00 
 
-wire [7:0] opm_dout;
-reg [7:0] rom_dout;
+// wire [7:0] opm_dout;
+// reg [7:0] rom_dout;
 
-wire woe_n = !( (!ce_n && !slt3_n) || !opm_n );  // IC104 OE_n ??
+assign oe_n = !(!ce_n && !slt3_n);  
 
 // Data bus is IO so this manages hight impedance for Output Enable
+/*
 always @(woe_n) begin
 	oe_n <= woe_n;
 	if( !woe_n )
@@ -66,6 +72,7 @@ always @(woe_n) begin
 	else
 		dio <= 8'hzz;
 end
+*/
 
 // assign midi_tx = 1'b0;
 
@@ -75,24 +82,36 @@ jt51(
 	.cs_n		( opm_n		),	// chip select
 	.wr_n		( wr_n		),	// write
 	.a0		( addr[0]	),
-	.d_in		( dio			), // data in
-	.d_out	( opm_dout	), // data out
+
+//	.d_in		( dio			), // data in
+//	.d_out	( opm_dout	), // data out
+	.d_in		( sfg_dbo	), // data in
+	.d_out	( sfg_dbi	), // data out
+
 	.irq_n	( int_n		),	// I do not synchronize this signal
 
+	// Low resolution output (same as real chip)
+	//output				sample,	// marks new output sample
+	//output	signed	[15:0] left,
+	//output	signed	[15:0] right,
+	
 	// Full resolution output
 	//output	signed	[15:0] xleft,
 	//output	signed	[15:0] xright,
-	.xleft 	( xleft),
-	.xright	(xright)
-
+	
 	// unsigned outputs for sigma delta converters, full resolution
-	// output	[15:0] dacleft,
-	// output	[15:0] dacright
+	//output	[15:0] dacleft,
+	//output	[15:0] dacright
+
+	//.xleft 	( left),
+	//.xright	(right)
+	.left 	( left),
+	.right	(right)
 );
 
-
-/*always @(*)
-	casex( addr[7:0], ce_n, slt3_n )
+/*
+always @(*)
+	casex( {addr[7:0], ce_n, slt3_n} )
 		{ 8'hxx, 1'b1, 1'bx } : rom_dout = 8'd0;
 		{ 8'hxx, 1'bx, 1'b1 } : rom_dout = 8'd0;
 		{ 8'h80, 1'b0, 1'b0 } : rom_dout = 8'h4d;
@@ -102,6 +121,7 @@ jt51(
 		{ 8'h84, 1'b0, 1'b0 } : rom_dout = 8'h4d;
 		{ 8'h85, 1'b0, 1'b0 } : rom_dout = 8'h30;
 		default: rom_dout = 8'd0;
-	endcase*/
+	endcase
+*/
 
 endmodule // jt_sfg01
